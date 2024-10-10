0f1aba963b486f74688fc406e96cd508a1db3425
flamaBarrita <mariobs_enrique@hotmail.com>
2024-10-10 14:24:40 -0600

    diseño formularios

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo")
+import express from "express";
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//Servidor
+
+const app = express();
+app.set("port",4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en el puerto", app.get("port"));
+
+//rutas
+
+app.get("/", (req,resp)=>resp.sendFile(__dirname + "/pages/login.html"));
