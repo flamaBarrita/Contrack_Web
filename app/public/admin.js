5f3f7d301260cbbe0fb30dbb1b37c88a5b18a757
flamaBarrita <mariobs_enrique@hotmail.com>
2024-12-03 01:00:54 -0600

    logout4

--- app/public/admin.js
+++ app/public/admin.js
@@ -1,12 +1,12 @@
-document.getElementsByTagName("button")[0].addEventListener("click", async (e) => {
+document.getElementsByTagName("button")[0].addEventListener("click", (e) => {
     e.preventDefault();
 
     // Hacer una solicitud al servidor para cerrar sesión
-    const response = await fetch("https://contrackweb-production.up.railway.app/api/logout", { method: "GET" });
+    const response =  fetch("https://contrackweb-production.up.railway.app/api/logout", { method: "GET" });
 
     if (response.ok) {
         // Redirigir al inicio de sesión o página principal
-        const data = await response.json();
+        const data = response.json();
         window.location.assign(data.redirect);
     } else {
         console.error("Error al cerrar sesión");
