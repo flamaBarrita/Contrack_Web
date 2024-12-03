7df44d53eeae3513ed2b1caea5378a3e419272e8
flamaBarrita <mariobs_enrique@hotmail.com>
2024-12-02 19:50:00 -0600

    index-regreso-2-bd

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise'; //conexión a la base de datos
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'mysql.railway.internal',
+    host: process.env.DB_HOST || 'junction.proxy.rlwy.net',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'xaveWluBuYHfErwWXMvoPiRRZCdJSXAe',
     database: process.env.DB_NAME || 'railway',
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 24236,
   });
 
 export default db;
